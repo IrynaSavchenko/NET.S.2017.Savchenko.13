161ff75ac3af0e4e07dbfadd1b90a0573a949bcb
phoenixIS <savchira9@gmail.com>
2017-08-03 08:16:49 +0300

    Add project files.

---
+++ Collections/Queue.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    public class Queue<T> : IEnumerable<T>
+    {
+        private const int DefaultSize = 100;
+        private const int ResizingPower = 2;
+
+        private T[] elements;
+        private int head;
+        private int tail;
+
+        public Queue() : this(DefaultSize) { }
+
+        public Queue(int size)
+        {
+            elements = new T[size];
+        }
+
+        public int Count { get; private set; }
+
+        public void Enqueue(T item)
+        {
+            if (ReferenceEquals(item, null))
+                throw new ArgumentNullException($"{nameof(item)} cannot be null");
+
+            if (Count == elements.Length) Resize();
+
+            if (tail == elements.Length - 1) tail = 0;
+
+            elements[tail] = item;
+            tail++;
+            Count++;
+        }
+
+        public T Dequeue()
+        {
+            if (Count == 0)
+                throw new InvalidOperationException("Cannot delete from an empty queue!");
+            T result = elements[head];
+            head++;
+            Count--;
+
+            if (head == elements.Length) head = 0;
+
+            return result;
+        }
+
+        public IEnumerator<T> GetEnumerator() => new QueueEnumerator<T>(this);
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        internal T this[int index] => elements[index];
+
+        private void Resize()
+        {
+            T[] newQueue = new T[elements.Length * ResizingPower];
+            if (head < tail) Array.Copy(elements, head, newQueue, 0, tail - head);
+            if (head > tail)
+            {
+                Array.Copy(elements, head, newQueue, 0, elements.Length - head);
+                Array.Copy(elements, 0, newQueue, elements.Length - head + 1, tail);
+            }
+
+            head = 0;
+            tail = Count;
+
+            elements = newQueue;
+        }
+    }
+}
---
+++ Collections/QueueEnumerator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Collections
+{
+    internal class QueueEnumerator<T> : IEnumerator<T>
+    {
+        private const int EmptyIndex = -1;
+
+        private Queue<T> queue;
+        private int currentIndex;
+
+        public QueueEnumerator(Queue<T> queue)
+        {
+            this.queue = queue;
+            currentIndex = EmptyIndex;
+
+        }
+
+        public T Current
+        {
+            get
+            {
+                if(currentIndex < 0 || currentIndex >= queue.Count())
+                    throw new IndexOutOfRangeException($"Index {nameof(currentIndex)} is out of range!");
+
+                return queue[currentIndex];
+            }
+        }
+
+        object IEnumerator.Current => Current;
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool MoveNext() => ++currentIndex < queue.Count;
+
+        public void Reset() => throw new NotSupportedException();
+    }
+}
